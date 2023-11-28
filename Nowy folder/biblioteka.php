a6187c9a6a5590c96d137c300bf5e57773d19915
dominikzych123 <tsmzysio@gmail.com>
2023-11-28 09:51:49 +0100

    ffff

--- Nowy folder/biblioteka.php
+++ Nowy folder/biblioteka.php
@@ -8,34 +8,37 @@
 </head>
 <body>
 	<div id="baner">
-		<h1>Biblioteka w Książkowicach wielkich </h1>
+		<h1>Biblioteka w Książkowicach Wielkich</h1>
 	</div>
 	<div id="lewy">
-    <?php
+		<h3> „Polecamy dzieła autorów:</h3>
+		<ul>
+		<?php
 		$con = mysqli_connect('localhost', 'root', '', 'biblioteka');
-		if(!empty($_POST['imie']) && !empty($_POST['nazwisko']) && !empty($_POST['rok'])) {
-			$imie = $_POST['imie'];
-			$nazwisko = $_POST['nazwisko'];
-			$rok = $_POST['rok'];
-			$kod = $imie[0].$imie[1].$rok[-2].$rok[-1].$nazwisko[0].$nazwisko[1];
-			$kod = strtolower($kod);
-			$kw1 = "INSERT INTO czytelnicy VALUES (NULL, '$imie', '$nazwisko', '$kod');";
-			mysqli_query($con, $kw1);
-			echo "Czytelnik: $imie $nazwisko został dodany do bazy danych";
+		$kw1 = "SELECT imie, nazwisko FROM autorzy;";
+		$res1 = mysqli_query($con, $kw1);
+		while($tab = mysqli_fetch_row($res1)) {
+			echo "<li>$tab[0] $tab[1]</li>";
 		}
 		?>
+		</ul>
 	</div>
 	<div id="srodkowy">
-		<img src="biblioteka.png" alt="biblioteka" />
+	
 		<h4>
 			ul. Czytelnicza 25<br/>
 			12-120 Książkowice<br/>
 			tel.: 123123123<br/>
-			e-mail: <a href="mailto:biuro@bib.pl">biuro@bib.pl</a>
+			 <a href="mailto:sekretariat@biblioteka.pl">napisz do nas</a>
+		    <img src="biblioteka.png" alt="książki" />
 		</h4>
+
+
+		
+	
 	</div>
 	<div id="prawy">
-    <h3>Dodaj czytelnika</h3>
+	<h3>Dodaj nowego czytelnika</h3>
 		<form action="biblioteka.php" method="post">
 			<label>
 				imię:
@@ -46,25 +49,37 @@
 				<input type="text" name="nazwisko" /><br/>
 			</label>
 			<label>
-				rok urodzenia:
+				symbol:
 				<input type="number" name="rok" /><br/>
 			</label>
 			<button>DODAJ</button>
 		</form>
-		<h3>Nasi czytelnicy</h3>
-		<ul>
 		<?php
-		$kw2 = "SELECT imie, nazwisko FROM czytelnicy;";
-		$res2 = mysqli_query($con, $kw2);
-		while($tab = mysqli_fetch_row($res2)) {
-			echo "<li>$tab[0] $tab[1]</li>";
+		$imie = $_POST['imie'];
+		$nazwisko = $_POST['nazwisko'];
+		$rok = $_POST['rok'];
+		if(!empty($_POST['imie']) && !empty($_POST['nazwisko']) && !empty($_POST['rok'])) {
+			$kod = $imie[0].$imie[1].$nazwisko[0].$nazwisko[1];
+			$kod = strtoupper($kod).$rok;
+			
+			$kw2 = "INSERT INTO czytelnicy VALUES (NULL, '$imie', '$nazwisko', '$kod');";
+			mysqli_query($con, $kw2);	
+			// echo "Czytelnik: $imie $nazwisko został dodany do bazy danych";
+			
+
 		}
+
 		mysqli_close($con);
 		?>
-		</ul>
+	</div>
+	<div id="prawydolny">
+		<?php
+		   echo "Czytelnik: $imie $nazwisko został dodany do bazy danych";
+		   ?>
+	
 	</div>
 	<div id="stopka">
-		<p>Projekt witryny: dominik zych</p>
+		<p>Projekt strony: dOMINIK zYCH</p>
 	</div>
 </body>
 </html>
